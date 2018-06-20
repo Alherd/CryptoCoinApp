4757d73991f5af5a669f8d20b570fa425216123c
Olgerd <bykalgerd@gmail.com>
2018-06-20 11:46:38 +0300

    1

---
+++ app/src/main/java/com/alherd/cryptocoinapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.alherd.cryptocoinapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
